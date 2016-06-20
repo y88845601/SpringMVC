08801a6490614e9dc23372768be8736be24278b5
y88845601 <xiaoxingyuan@126.com>
2016-06-20 14:55:44 +0800

    Update SmallMethods.java
    增加获取IP地址、取properties文件中的值

--- springMVC/src/com/xxy/util/SmallMethods.java
+++ springMVC/src/com/xxy/util/SmallMethods.java
@@ -9,6 +9,84 @@ package com.xxy.util;
 public class SmallMethods {
 	
 	/**
+	 * 获取请求者的IP
+	 * 修改者名字   xxy
+	 * 修改日期   2016-4-11
+	 * 修改内容
+	 * @param @param request
+	 * @param @return
+	 * @return String
+	 */
+	public static String getIpAddress(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+	
+	/**
+	 * 根据IP获取MAC地址(内网测试通过,外网未测试,不稳定尽量不要使用)
+	 * 修改者名字   xxy
+	 * 修改日期   2016-4-12
+	 * 修改内容
+	 * @param @param ip
+	 * @param @return
+	 * @return String
+	 */
+	public static String getMACAddress(String ip) {
+		String str = "";
+		String macAddress = "";
+		try {
+			Process p = Runtime.getRuntime().exec("nbtstat -A " + ip);
+			InputStreamReader ir = new InputStreamReader(p.getInputStream());
+			LineNumberReader input = new LineNumberReader(ir);
+			for (int i = 1; i < 100; i++) {
+				str = input.readLine();
+				if (str != null) {
+					if (str.indexOf("MAC Address") > 1) {
+						macAddress = str.substring(
+								str.indexOf("MAC Address") + 14, str.length());
+						break;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return macAddress;
+	}
+	
+	/**
+	 * 从API配置文件中拿到对应的参数值
+	 * 修改者名字   xxy
+	 * 修改日期   2016-4-11
+	 * 修改内容
+	 * @param @param key
+	 * @param @return
+	 * @return String
+	 */
+	public static String getProperties(String key){
+		String val = null;
+		Properties property = new Properties();
+		try {
+			String rootUrl = SmallMethods.class.getClassLoader().getResource("").getPath();
+			//Constants.propertiesURL为properties文件储存的路径
+			property.load(new FileInputStream(rootUrl+Constants.propertiesURL));//拿到API配置文件
+			val = property.getProperty(key);//从配置文件中拿到对应key的值
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return val;
+	}
+	
+	/**
 	 * 将数组内的值变成大写
 	 * @author xingyuan
 	 * @date 2016-4-13
